a7075ac2da8ac8e4898bbc9ea448b1407d432728
Samir Katurde <samir.katurde@raweng.com>
2018-01-31 18:37:29 +0530

    Created Custom NPM

---
+++ Project/test.js
@@ -0,0 +1,6 @@
+console.log("=========================")
+var js = require('../Create NPM/index.js').printMsg()
+js
+// var js = require('../Create NPM/index.js')
+// js.printMsg()
+console.log("=========================")
